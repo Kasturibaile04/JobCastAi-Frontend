e4681443983399d4ee0113e990a8b8501275febc
Kasturibaile04 <infokasturi23@gmail.com>
2026-07-11 01:47:15 +0530

    Update auth.api.js

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://job-cast-ai-frontend.vercel.app",
     withCredentials: true
 })
 
